6dbee3b2ab4d098a3dce2e4c83fa81526ad13b85
kavana <skavana2003@gmail.com>
2026-02-03 12:23:42 +0530

    Removed extra Day 2 task files

--- day2task2.py
+++
@@ -1,4 +0,0 @@
-total_bill_amount = float(input("enter the total amount spent in restaurant"))
-number_of_people = input("enter number of people to split the bill")
-bill_per_person = float(total_bill_amount) / int(number_of_people)
-print("each person should pay:", bill_per_person)
--- day2task3.py
+++
@@ -1,5 +0,0 @@
-item_name="laptop"
-quantity=2
-price_per_item=1500.00
-in_stock=True
-print("item:",item_name, "quantity:", quantity, "price per item:", price_per_item, "in stock:", in_stock)
