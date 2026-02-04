ca3c42ca8ddab5cb8d00949e92f16588ab0341a6
kavana <skavana2003@gmail.com>
2026-02-04 13:38:30 +0530

    day 3 completed

---
+++ day3.py
@@ -0,0 +1,30 @@
+inventory = ["Apples", "Bananas", "Carrots", "Dates"]
+print("Current Inventory:", inventory)
+
+inventory.append("Eggs")
+print("Updated Inventory:", inventory)
+
+inventory.remove("Bananas")
+print("Final Inventory:", inventory)
+
+inventory.sort()
+print("Sorted Inventory:", inventory)
+
+
+temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+print(temperatures[0], temperatures[-1])
+
+print("afternoon peak",temperatures[3:6])
+
+print("last 3 hours",temperatures[-3:])
+
+
+screen_res = (1920, 1080)
+print("Current Resolution: 1920x1080")
+
+screen_res = (1920, 1080)
+print("Current Resolution: 1920x1080")
+
+
+print("Tuples cannot be modified!")
+
