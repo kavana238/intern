0472e4782b977140854e14dc6814065e58b60c90
kavana <skavana2003@gmail.com>
2026-02-06 11:52:37 +0530

    day 5 completed

---
+++ day5/task2/main.py
@@ -0,0 +1,10 @@
+import math_operations
+
+base = 2
+power = 10
+result = math_operations.power(base, power)
+
+average = math_operations.average([10, 20, 30,40])
+
+print(f"{base} raised to the power of {power} is {result}")
+print(f"The average of the list is {average}")
---
+++ day5/task2/math_operations.py
@@ -0,0 +1,8 @@
+def power(base, exponent):
+    return base ** exponent
+
+
+def average(numbers_list):
+    return sum(numbers_list) / len(numbers_list)
+
+   
