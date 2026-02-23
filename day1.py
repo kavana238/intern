58afab620f17a5938a76c0791f0f1cea4563634b
kavana <skavana2003@gmail.com>
2026-02-23 15:55:40 +0530

    Cleaned conflict markers properly

--- day1.py
+++ day1.py
@@ -3,9 +3,3 @@ print("Hello, World!")
 print("python version:", sys.version)
 print("Hello")
 print("This is FEATURE branch")
-<<<<<<< HEAD
-
-print("Hello")
-=======
-print("This is FEATURE branch")
->>>>>>> feature-viz
